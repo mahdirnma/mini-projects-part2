4fb6c007dc444718065b729769a6dc486c4541c5
mahdirnma <rahnamamahdi78@gmail.com>
2025-03-12 20:48:27 +0330

    create api for add category

--- manage-blogs2/app/Http/Controllers/Api/CategoryController.php
+++ manage-blogs2/app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use App\Services\ApiResponseBuilder;
@@ -27,9 +28,14 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $result=$this->categoryService->addCategory($request->all());
+        $apiResponse=$result->success?
+            (new ApiResponseBuilder())->message('tag added successfully')->data(new CategoryResource($result->data)):
+            (new ApiResponseBuilder())->message('tag added unsuccessfully')->data($result->data);
+        return $apiResponse->response();
+
     }
 
     /**
