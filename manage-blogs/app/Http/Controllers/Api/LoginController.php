802f96da807381625abd0ca11b12c92efdd80391
mahdirnma <rahnamamahdi78@gmail.com>
2025-03-12 18:36:00 +0330

    start manage-blogs2 project

---
+++ manage-blogs/app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreLoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function __invoke(StoreLoginRequest $request)
+    {
+        if (!Auth::attempt($request->validated())) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        $token=Auth::user()->createToken('authToken')->plainTextToken;
+        return response()->json([
+            'user'=>Auth::user(),
+            'token'=>$token,
+        ]);
+    }
+}
